95651d3f2c720fcaff38a54704a7195ecc5c6cb0
nori266 <noriel266@gmail.com>
2023-01-10 22:23:44 +0200

    added buttons and authorization

--- bot.py
+++ bot.py
@@ -8,17 +8,34 @@ import db_operations as db
 
 load_dotenv()
 TOKEN = environ.get('TOKEN')
+ADMIN_USERNAME = environ.get('ADMIN_USERNAME')
 
 bot = telebot.TeleBot(TOKEN)
+
+markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+itembtn1 = telebot.types.KeyboardButton('Send me later')
+# TODO if status Open, then 'working on it', if in_progress, then 'done'
+itembtn2 = telebot.types.KeyboardButton('Working on it')
+# itembtn2 = telebot.types.KeyboardButton('Done')
+itembtn3 = telebot.types.KeyboardButton('Not relevant')
+markup.add(itembtn1, itembtn2, itembtn3)
 
 
 @bot.message_handler(commands=['start'])
 def send_random_note(message):
-    thought = db.get_random_note()
-    bot.send_message(
-        message.from_user.id,
-        f"Revisit this thought: \n{thought}",
-    )
+    user = message.from_user
+    if user.username == ADMIN_USERNAME:
+        thought = db.get_random_note()
+        bot.send_message(
+            user.id,
+            f"Random thought from your pull: \n{thought}",
+            reply_markup=markup
+        )
+    else:
+        bot.send_message(
+            user.id,
+            f"You are not authorized to use this bot. Please contact @{ADMIN_USERNAME} to get access.",
+        )
 
 
 if __name__ == '__main__':
