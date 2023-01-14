e1c55bc0f343ce3a53362ea02914ca1719913881
nori266 <noriel266@gmail.com>
2023-01-14 20:46:15 +0200

    bot handler for adding new note

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ from os import environ
 from dotenv import load_dotenv
 import telebot
 
-from bert_clf import model
 import db_operations as db
 
 
@@ -23,7 +22,9 @@ markup.add(itembtn1, itembtn2, itembtn3)
 
 db_connection = db.get_connection()
 
-print(model.config)
+add_new_command_state = False
+default_prompt = "Please use /new command to add new thought to your pull " \
+                 "or /note to get a random thought from your pull."
 
 
 @bot.message_handler(commands=['note'])
@@ -36,6 +37,45 @@ def send_random_note(message):
             f"Random thought from your pull: \n{thought}",
             reply_markup=markup
         )
+    else:
+        bot.send_message(
+            user.id,
+            f"You are not authorized to use this bot. Please contact @{ADMIN_USERNAME} to get access.",
+        )
+
+
+@bot.message_handler(commands=['new'])
+def add_new_note(message):
+    user = message.from_user
+    if user.username == ADMIN_USERNAME:
+        global add_new_command_state
+        add_new_command_state = True
+        bot.send_message(
+            user.id,
+            f"Please send me a new thought to add to your pull.",
+        )
+    else:
+        bot.send_message(
+            user.id,
+            f"You are not authorized to use this bot. Please contact @{ADMIN_USERNAME} to get access.",
+        )
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    user = message.from_user
+    if user.username == ADMIN_USERNAME:
+        if add_new_command_state:
+            db.add_thought(db_connection, message.text, 'todo', 'week', 1)
+            bot.send_message(
+                user.id,
+                f"New thought added to your pull.",
+            )
+        else:
+            bot.send_message(
+                user.id,
+                default_prompt,
+            )
     else:
         bot.send_message(
             user.id,
