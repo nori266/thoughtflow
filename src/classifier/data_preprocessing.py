6f467cae2254d01bc5a7b06ce832b226bab8f87e
nori266 <noriel266@gmail.com>
2023-01-26 15:03:04 +0200

    changed data format in train test file names

--- src/classifier/data_preprocessing.py
+++ src/classifier/data_preprocessing.py
@@ -12,7 +12,10 @@ data = pd.read_csv('data/all_thoughts.csv')
 train, test = train_test_split(data, test_size=0.1, random_state=0)
 
 now = datetime.datetime.now()
-date = now.strftime("%Y-%m-%d")
+date = now.strftime("%y%m%d")
 
-train[["thought", "class"]].to_csv(f'data/{date}_train.csv', index=False)
-test[["thought", "class"]].to_csv(f'data/{date}_test.csv', index=False)
+train[["thought", "label"]].to_csv(f'data/train_test_data/{date}_train.csv', index=False)
+test[["thought", "label"]].to_csv(f'data/train_test_data/{date}_test.csv', index=False)
+
+print("Train data shape:", train.shape)
+print("Test data shape:", test.shape)
