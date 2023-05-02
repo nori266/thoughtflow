8366db1c65985a1542167f5c94e8c1334d2cc127
nori266 <noriel266@gmail.com>
2023-05-02 14:47:12 +0300

    updated requirements, fixed test

--- test/test_db_action_handler.py
+++ test/test_db_action_handler.py
@@ -19,10 +19,9 @@ def test_show_last_n():
 
 def test_send_plots():
     action_handler = DBActionHandler()
-    thoughts_last_month = action_handler.send_plots()
-    assert isinstance(thoughts_last_month, list)
-    assert len(thoughts_last_month) > 0  # TODO might be empty if no thoughts were added last month
-    assert isinstance(thoughts_last_month[0], tuple)
+    plot_file = action_handler.send_plots()
+    assert isinstance(plot_file, str)
+    assert plot_file.endswith('.png')
 
 
 if __name__ == '__main__':
