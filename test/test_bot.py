6b23e8f2afa38d2ab01fbd8c2b34ea3e93b5be9c
nori266 <noriel266@gmail.com>
2023-07-03 15:39:03 +0300

    fixed test

--- test/test_bot.py
+++ test/test_bot.py
@@ -5,7 +5,7 @@ import sys
 sys.path.append('src')
 
 # FIXME: how to import from src.bot.py properly?
-from bot import load_classifier, get_buttons, send_random_note
+from bot import get_buttons, default_keyboard, send_random_note
 
 # test functions from src.bot.py
 # TODO how to import from src.bot.py?
