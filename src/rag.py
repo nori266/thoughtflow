7f10729a5b0d5f7773eb00da76ffcc4d4b24b9de
akozlova <anna.kozlova.ext@nokia.com>
2023-12-25 16:23:23 +0200

    incorporate candidate categories into prompt

--- src/rag.py
+++ src/rag.py
@@ -18,13 +18,15 @@ class RAG:
     def __init__(self):
         self.model_name = "orca2:13b"  # orca2 is best
         self.llm = Ollama(model=self.model_name)  # TODO switch to deepinfra llama
-        self.embedding_function = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")  # TODO experiment with other embeddings
+        embedding_function = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")  # TODO experiment with other embeddings
         self.db_action_handler = DBActionHandler()
 
         # Load data and categories
         self.data = self.db_action_handler.get_all_notes()  # TODO just store data in vectors
         with open("data/category_paths.csv") as f:
-            self.categories = f.read()  # TODO use only most similar categories
+            categories = [
+                c.strip() for c in f
+            ]
 
         self.examples = [
             {
@@ -32,39 +34,47 @@ class RAG:
                 'output': row.label,
             } for row in self.data
         ]
-
         self.prompt = PromptTemplate(
             input_variables=["input", "output"],
             template="Example input: {input},\nExample output: {output}"
         )
-
         self.example_selector = SemanticSimilarityExampleSelector.from_examples(
             self.examples,
-            embeddings=self.embedding_function,
+            embeddings=embedding_function,
             vectorstore_cls=Chroma,
             k=5
         )
+        # TODO check if db exists
+        # self.db = Chroma.from_texts(categories, embedding_function, persist_directory="./chroma_categories")
 
-        self.prefix = (
+        # load from disk
+        self.db = Chroma(persist_directory="./chroma_categories", embedding_function=embedding_function)
+
+        prefix = (
             "I want you to categorize my todo notes according to high-level goals and corresponding activities. "
             "If the input note is not a todo, assign it a category 'Note'. If there's no suitable category you can "
             "create a new one or use a higher-level category going up in the tree. Answer with the category ONLY, the "
-            "most suitable one."
+            "most suitable one.\n"
+            "Here are some candidate categories:\n{candidate_categories}\n\n"
+            "Here are some examples:"
         )
 
         self.similar_prompt = FewShotPromptTemplate(
             example_selector=self.example_selector,
             example_prompt=self.prompt,
-            prefix=self.prefix,
+            prefix=prefix,
             suffix="Input: {note}\n",
-            input_variables=["note"]
+            input_variables=["note", "candidate_categories"]
         )
 
     def predict(self, message: str) -> Dict[str, Union[str, float]]:
-        whole_prompt = self.similar_prompt.format(note=message)
+        # TODO why are there duplicates in sim search results?
+        candidate_categories = {doc.page_content for doc in self.db.similarity_search(message, k=10)}
+        candidate_categories_text = "\n".join(candidate_categories)
+        whole_prompt = self.similar_prompt.format(note=message, candidate_categories=candidate_categories_text)
         LOGGER.info(whole_prompt)
         llm_output = self.llm(whole_prompt)
-        print(llm_output)
+        LOGGER.info(f"llm output: {llm_output}")
         return self.post_process_prediction(llm_output, message)
 
     def post_process_prediction(self, prediction_text: str, message: str) -> Dict[str, Union[str, float]]:
